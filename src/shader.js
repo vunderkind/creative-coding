433892e20ef4d8fe4a4a3a2325aea939aa403988
mogwai <feelirabs@gmail.com>
2020-05-10 23:54:14 +0100

    Add step function

--- src/shader.js
+++ src/shader.js
@@ -9,16 +9,15 @@ const settings = {
 };
 
 // Your glsl code
-const frag = glsl(`
+const frag = glsl(/* glsl */`
   precision highp float;
 
   uniform float time;
   varying vec2 vUv;
 
   void main () {
-    vec3 color = 0.5 + 0.5 * cos(time + vUv.xyx + vec3(0.0, 2.0, 4.0));
-    gl_FragColor = vec4(color, 1.0);
-  }
+    gl_FragColor = vec4(1.0, 1.0,0.0,1.0);
+   }
 `);
 
 // Your sketch, which simply returns the shader

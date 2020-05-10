23605b113d5fd94569e53f64f32071a14abc60bb
mogwai <feelirabs@gmail.com>
2020-05-11 00:23:59 +0100

    Add glsl-noise

--- src/shader.js
+++ src/shader.js
@@ -20,18 +20,22 @@ const frag = glsl(/* glsl */`
   uniform float aspect;
   varying vec2 vUv;
 
+  #pragma glslify: 
+
 
   void main () {
-    vec3 colorA = sin(time) + vec3(1,0.0,0.0);
-    vec3 colorB = vec3(0.0,1.0,0.0);
-    vec2 center = vUv - 0.5;
-    center.x*=aspect;
-    float dist = length(center);
+    // vec3 colorA = sin(time) + vec3(1,0.0,0.0);
+    // vec3 colorB = vec3(0.0,1.0,0.0);
+    // vec2 center = vUv - 0.5;
+    // center.x*=aspect;
+    // float dist = length(center);
 
-    float alpha = smoothstep(0.25075, 0.25, dist);
+    // float alpha = smoothstep(0.25075, 0.25, dist);
 
-    vec3 color = mix(colorA, colorB, vUv.x + vUv.y * sin(time));
-    gl_FragColor = vec4(color,alpha);
+    // vec3 color = mix(colorA, colorB, vUv.x + vUv.y * sin(time));
+    // gl_FragColor = vec4(color,alpha);
+
+      gl_FragColor = vec4(vec3(vUv.x),1.0);
    }
 `);
 
